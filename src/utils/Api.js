814d6c86c5b95e77536d0b6f9d65bad04d4e17d9
Junhyun <mnmthatisus@gmail.com>
2021-12-18 12:00:22 +0900

    api url change

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -1,1 +1,2 @@
-export const ProductsAPI = "http://localhost:8888/api/nike-store";
+// export const ProductsAPI = "http://localhost:8888/api/nike-store";
+export const ProductsAPI = "https://yello-sneakers.netlify.app/";
