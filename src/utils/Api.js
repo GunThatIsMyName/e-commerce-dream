1953bf8bc51ae360642d50013139b852df482421
Junhyun <mnmthatisus@gmail.com>
2021-12-18 12:23:11 +0900

    url dot missing

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -1,2 +1,2 @@
 // export const ProductsAPI = "http://localhost:8888/api/nike-store";
-export const ProductsAPI = "/netlify/functions/nike-store";
+export const ProductsAPI = "/.netlify/functions/nike-store";
